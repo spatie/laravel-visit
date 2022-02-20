99e5f19a2b52a4836049797724f17bfd496cc18e
freekmurze <freekmurze@users.noreply.github.com>
2022-02-20 19:01:03 +0000

    Fix styling

--- tests/VisitCommandTest.php
+++ tests/VisitCommandTest.php
@@ -2,15 +2,14 @@
 
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-use function Pest\Laravel\artisan;
 
-beforeEach(function() {
+beforeEach(function () {
     Route::get('/', function () {
         return 'get result';
     });
 });
 
-it('can visit a page', function() {
+it('can visit a page', function () {
     Artisan::call('visit /');
     expect(true)->toBe(true);
 
