3130ecb94a26bcc17e2a527f40ac26652ac4f098
Freek Van der Herten <freek@spatie.be>
2022-02-24 19:53:21 +0100

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,7 +11,7 @@ use Termwind\Laravel\TermwindServiceProvider;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
